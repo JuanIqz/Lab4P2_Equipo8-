b0983bd27860e39597ef509de2b72f1b18ceff13
Gael2019 <ramiritokillu@gmail.com>
2023-08-11 14:12:04 -0600

    prueba 2

---
+++ JavaApplication5/src/javaapplication5/Pokemon.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication5;
+
+/**
+ *
+ * @author Jorge Ramirez
+ */
+public class Pokemon {
+    String especie;
+    int vida;
+    int ataque;
+    int defensa;
+    
+    
+}
