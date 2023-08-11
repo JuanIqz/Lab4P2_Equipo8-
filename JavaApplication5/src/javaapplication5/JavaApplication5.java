1feceaea15ce92ae94db9efdcb10432af213b80d
JuanIqz <juanferiqz7@gmail.com>
2023-08-11 14:08:41 -0600

    Menu principal terminado

--- JavaApplication5/src/javaapplication5/JavaApplication5.java
+++ JavaApplication5/src/javaapplication5/JavaApplication5.java
@@ -4,6 +4,8 @@
  */
 package javaapplication5;
 
+import java.util.Scanner;
+
 /**
  *
  * @author juanf
@@ -14,7 +16,45 @@ public class JavaApplication5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       Scanner pd= new Scanner(System.in);
+       boolean bandi=true;
+        while (bandi) {            
+            
+        
+        System.out.println("Bienvenido al sistema de juego Pokemon");
+        System.out.println("Sus opciones son los siguientes");
+        System.out.println();
+        System.out.println("1.Registrar entrenador");
+        System.out.println("2.Combatir");
+        System.out.println("3.Capturar y entrenar");
+        System.out.println("4.agregar movimientos");
+        System.out.println("5.salir");
+        System.out.println();
+        System.out.println("Marque el numero de la opcion para seleccionarla");
+        int menu=pd.nextInt();
+        switch (menu) {
+            case 1:
+            
+                break;
+            case 2:
+
+                break;
+            case 3:
+                
+                break;
+            case 4:
+
+                break;
+            case 5:
+                System.out.println("Saliendo...");
+                bandi=false;
+                break;
+            default:
+                System.out.println("Opcion invalida, por favor seleccione otra");
+                break;
+        }
+        }
+        
     }
     
 }
